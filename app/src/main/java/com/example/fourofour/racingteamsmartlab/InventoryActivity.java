147659542f11bd2b683bbf615a6d0d0d9792633b
yash-s20 <yash.sharma200999@gmail.com>
2018-06-04 14:27:14 +0530

    Fixed a few button id's, testing out the commit

--- app/src/main/java/com/example/fourofour/racingteamsmartlab/InventoryActivity.java
+++ app/src/main/java/com/example/fourofour/racingteamsmartlab/InventoryActivity.java
@@ -9,5 +9,7 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        //TODO Connect the google sheets api to this
+
     }
 }
