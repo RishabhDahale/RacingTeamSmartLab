c5917731d5771b3f60726dc2a631532988085958
RishabhDahale <33431521+RishabhDahale@users.noreply.github.com>
2018-06-07 12:25:04 +0530

    Notification messages going on cloud again

--- app/src/main/java/com/example/fourofour/racingteamsmartlab/NotificationActivity.java
+++ app/src/main/java/com/example/fourofour/racingteamsmartlab/NotificationActivity.java
@@ -180,6 +180,7 @@ public class NotificationActivity extends AppCompatActivity {
             }
         };
         mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        
 
     }
 
