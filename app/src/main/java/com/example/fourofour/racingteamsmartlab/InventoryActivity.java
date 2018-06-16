7713fc0ffc931be4360e35e67ec4efae9dc96b67
yash-s20 <yash.sharma200999@gmail.com>
2018-06-16 11:18:42 +0530

    inventory access working

--- app/src/main/java/com/example/fourofour/racingteamsmartlab/InventoryActivity.java
+++ app/src/main/java/com/example/fourofour/racingteamsmartlab/InventoryActivity.java
@@ -93,6 +93,7 @@ public class InventoryActivity extends Activity
             }
         });
         activityLayout.addView(mCallApiButton1);
+        
 
         mOutputText = new TextView(this);
         mOutputText.setLayoutParams(tlp);
