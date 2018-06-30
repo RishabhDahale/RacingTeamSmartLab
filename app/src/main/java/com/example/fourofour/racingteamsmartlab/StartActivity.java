487bf9d7b359424eb14f283dbc65091e4e7f6fdc
RishabhDahale <33431521+RishabhDahale@users.noreply.github.com>
2018-06-30 23:03:30 +0530

    Some changes in notification code...

--- app/src/main/java/com/example/fourofour/racingteamsmartlab/StartActivity.java
+++ app/src/main/java/com/example/fourofour/racingteamsmartlab/StartActivity.java
@@ -1,25 +1,17 @@
 package com.example.fourofour.racingteamsmartlab;
 
-import android.annotation.SuppressLint;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.api.services.sheets.v4.model.Color;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -38,6 +30,9 @@ public class StartActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static long viewedNotiNum=0;
+    public static long onFirebaseNoti;
+
 
     public static String getUserName() {
         return mUsername;
@@ -102,7 +97,6 @@ public class StartActivity extends AppCompatActivity {
                 inLab= !inLab;
             }
         });
-
 
 
     }
@@ -170,8 +164,6 @@ public class StartActivity extends AppCompatActivity {
         }
 
     }
-
-
 
 }
 
